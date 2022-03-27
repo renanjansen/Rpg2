a25d80cf9aede42d57df348102f2f5cf8fa8a9e1
Renan Jansen <renanjansen@gmail.com>
2022-03-27 10:53:31 -0300

    feat(primeiro commit)
    - Primeiro commmit de projeto de jogo de RPG utilizando o paradigmaorientado a objeto.

---
+++ Personagens/Ladra.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require_once './Personagem.php';
+
+$ladra = new Personagem();
+$ladra->nome = "Aysha";
+$ladra->vida = 60;
+$ladra->ataque = 10;
+$ladra->esquiva = 8;
+$ladra->defesa = 4;
+$ladra->especialNome = 'Facada';
+$ladra->imagem= 'https://st.depositphotos.com/2458365/3409/i/600/depositphotos_34097931-stock-photo-dark-skinned-girl-warrior.jpg';
+$ladra->especialImagem = 'https://c.tenor.com/P2Ou4PzoxkMAAAAS/dio-knives.gif';
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    
+    require 'Combate.php';
+    
+    $herois = [$mago, $ladra, $guerreiro];
+    $luta1 = new Combate;
+    
+
+
+    echo "<PRE>";
+    foreach($herois as $heroi){
+        var_dump($heroi);
+    }
+    var_dump($eskeleton);
+    
+
+    $luta1->turn($mago, $guerreiro);
+
+    //var_dump($mago->jogarDado());
+    //var_dump($luta);
