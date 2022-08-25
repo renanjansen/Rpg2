01c70f546fe8e4d3d48f6b9ceb8fdef702b2fcb8
Renan Jansen <renanjansen@gmail.com>
2022-08-25 10:33:47 -0300

    Início da customização do front

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php
     
-    require 'Combate.php';
+   
+    require './Componentes/menu.html';
+    require './Componentes/card.php';
+    require './Componentes/footer.html';
     
-    $herois = [$mago, $ladra, $guerreiro];
-    $luta1 = new Combate;
     
-
-
-    echo "<PRE>";
-    foreach($herois as $heroi){
-        var_dump($heroi);
-    }
-    var_dump($eskeleton);
     
-
-    $luta1->turn($mago, $guerreiro);
-
-    //var_dump($mago->jogarDado());
-    //var_dump($luta);
